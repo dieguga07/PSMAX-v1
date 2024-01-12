cf8c9c752d033f736eceba983bd8f7bd800c84b7
diego gutierrez <120743511+dieguga07@users.noreply.github.com>
2024-01-12 11:58:10 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,1 @@
-import React from 'react'
-import LoginForm from '../components/LoginForm'
 
-const Login = () => {
-  return (
-    <section>
-
-      <LoginForm/>
-
-    </section>
-  )
-}
-
-export default Login
