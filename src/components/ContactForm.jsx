48691633cccd6e88c51b5433c480c4c4091d1bce
diego gutierrez <120743511+dieguga07@users.noreply.github.com>
2024-01-12 11:53:01 +0100

    Update ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../context/UserContext'
 
 const ContactForm = () => {
 
@@ -6,21 +7,85 @@ const ContactForm = () => {
 
     const [user, setUsernombre] = useState("")
     const [aportacion, setAportacion] = useState("")
-  
+    const [email, setEmail] = useState("")
+   
     const [mostrarToast, setMostrarToast] = useState(false)
     const [mensajeToast, setMensajeToast] = useState("")
 
-    const handleContact = (()=>{
+    const handleContacto = (()=>{
 
-        if (user.trim() === "" || aportacion.trim() === "") {
+        if (user.trim() === "" || aportacion.trim() === "" || email.trim() ==='') {
             Toast('Por favor, complete todos los campos')
             return
           }
+
+          const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+        if (!emailRegex.test(email)) {
+            Toast('El correo electrónico no es correcto o el formato es erróneo')
+            return
+          }
+
+          setUsernombre("")
+          setAportacion("")
+          setEmail("")
+
+          Toast('Enviando datos...')
     })
 
 
+    const Toast = (mensaje) => {
+      setMensajeToast(mensaje)
+      setMostrarToast(true)
+  
+      setTimeout(() => {
+       
+        setMostrarToast(false)
+      }, 3000)
+    }
+
   return (
-    <div>ContactForm</div>
+    <>
+
+    <article className="container">
+
+      <form>
+
+        <h2>Contactanos</h2>
+
+        <label>Usuario:</label>
+        <input typeof='text' value={user} onChange={(e) => setUsernombre(e.target.value)}/>
+
+        <label>Correo electrónico:</label>
+        <input typeof='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+
+        <label>Aportacion:</label>
+        <textarea value={aportacion} onChange={(e) => setAportacion(e.target.value)} />
+        
+        <button type="button" onClick={handleContacto}>Enviar</button>
+
+        {
+         
+
+          
+        }
+
+      </form>
+
+    </article>
+
+    {/* La expresión {mostrarToast && (...)} se utiliza para condicionalmente renderizar el bloque JSX dentro de los paréntesis 
+    solo si mostrarToast es true. De lo contrario, no se renderiza nada.Si intentara poner {mostrarToast(...)} , estaría tratando de 
+    invocar mostrarToast directamente en vez de un elemento JSX */}
+
+    {mostrarToast && (
+      <div className="toast">
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        <p>{mensajeToast}</p>
+      </div>
+    )}
+
+  </>
   )
 }
 
